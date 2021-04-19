1fd79ff963839d1a6511300a40f6d0f0b17d0a09
David Every <David_every.ovr@hotmail.com>
2021-04-19 13:33:09 +0100

    scion feedback

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,10 @@ that flexes into rows.
 and will add additional placeholder images to round to the nearest 4
 */
 
+$(document).ready(function(){
+
+    /*! Fades in page on load */
+    $('body').css('display', 'none');
+    $('body').fadeIn(500);
+    
+    });
