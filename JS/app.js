bea528c05609b64c5a2d6d1d41660b6f1b93b13f
David Every <David_every.ovr@hotmail.com>
2021-04-19 09:19:36 +0100

    Minor position fixes in header

--- JS/app.js
+++ JS/app.js
@@ -8,52 +8,52 @@ $('document').ready(function(){
     $("#head").load("/htmlToCopy/htmlToCopy.html");
 });
 
-let textDisplay = document.getElementById("h1");
+// let textDisplay = document.getElementById("h1");
 
-const title =
-[
-    "My name is David Every ",
-    "i am a web Developer"
-]
-const append = document.createTextNode("|");
+// const title =
+// [
+//     "My name is David Every ",
+//     "i am a web Developer"
+// ]
+// const append = document.createTextNode("|");
 
-let i = 0;
-let j = 0;
-let currentPhrase = [];
+// let i = 0;
+// let j = 0;
+// let currentPhrase = [];
 
 
-function writeTitle(){
+// function writeTitle(){
 
-    textDisplay.innerHTML = currentPhrase.join('');
-    if(i < title.length){
-        if(j < title[i].length){
-            currentPhrase.push(title[i][j]);
-            j++;
-        }
-        if(j == title[i].length){
-            i++;
-        }
-    }
-    //If the title has been completed run idle anim
-    if(i == 1){
-        // console.log("i = 1");
+//     textDisplay.innerHTML = currentPhrase.join('');
+//     if(i < title.length){
+//         if(j < title[i].length){
+//             currentPhrase.push(title[i][j]);
+//             j++;
+//         }
+//         if(j == title[i].length){
+//             i++;
+//         }
+//     }
+//     //If the title has been completed run idle anim
+//     if(i == 1){
+//         // console.log("i = 1");
 
-        blink();
-        //get the cursor to blink at the end of the last letter
+//         blink();
+//         //get the cursor to blink at the end of the last letter
 
-    } else{
-        setTimeout(writeTitle,100);
+//     } else{
+//         setTimeout(writeTitle,100);
 
-    }
+//     }
     
-}
+// }
 
-function blink(){
-    let blink = window.getComputedStyle(textDisplay,'::after');
-    let color = blink.getPropertyValue('color');
-    console.log(color);
-    color.innerHTML.style.color("blue");
-    // setTimeout(remove,300);
-}
+// function blink(){
+//     let blink = window.getComputedStyle(textDisplay,'::after');
+//     let color = blink.getPropertyValue('color');
+//     console.log(color);
+//     color.innerHTML.style.color("blue");
+//     // setTimeout(remove,300);
+// }
 
-writeTitle();
+// writeTitle();
